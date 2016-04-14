7a2f9c4ce1d4bcc2e89f4609fcc5ef83b3614059
Michael Madden <mikemadden42@icloud.com>
2016-04-14 16:39:29 -0500

    Initial version

---
+++ delta.cc
@@ -0,0 +1,27 @@
+// Build with cmake, make, ninja:
+// rm -rf release/; mkdir release; cd release/; cmake -GNinja ..; ninja
+// rm -rf release/; mkdir release; cd release/; cmake ..; make
+
+// Build without cmake:
+// g++ -Wall -O2 -mtune=native -o delta delta.cc -lboost_date_time
+// clang++ -Wall -O2 -mtune=native -o delta delta.cc -lboost_date_time
+
+#include <iostream>
+#include "boost/date_time/gregorian/gregorian.hpp"
+
+int main() {
+  using namespace boost::gregorian;
+  std::string s = "2016-03-30";
+  try {
+    date start(from_simple_string(s));
+    date today = day_clock::local_day();
+    days diff = today - start;
+    std::cout << "now: " << today << std::endl;
+    std::cout << "then: " << start << std::endl;
+    std::cout << "diff: " << diff.days() << " days" << std::endl;
+  }
+  catch (...) {
+    std::cout << "bad date: " << s << std::endl;
+  }
+  return 0;
+}
