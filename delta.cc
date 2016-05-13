6686590f0c50928f6d2722eb2ed8067d0871f23b
Michael Madden <mikemadden42@icloud.com>
2016-05-13 15:48:28 -0500

    Create diffDays function.

--- delta.cc
+++ delta.cc
@@ -1,27 +1,18 @@
-// Build with cmake, make, ninja:
-// rm -rf release/; mkdir release; cd release/; cmake -GNinja ..; ninja
-// rm -rf release/; mkdir release; cd release/; cmake ..; make
-
-// Build without cmake:
-// g++ -Wall -O2 -mtune=native -o delta delta.cc -lboost_date_time
-// clang++ -Wall -O2 -mtune=native -o delta delta.cc -lboost_date_time
-
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <iostream>
 
+using namespace boost::gregorian;
+
+int diffDays(std::string s1, std::string s2) {
+  date start(from_simple_string(s1));
+  date end(from_simple_string(s2));
+  days diff = start - end;
+  return std::abs(diff.days());
+}
+
 int main() {
-  using namespace boost::gregorian;
-  std::string s = "2016-03-30";
-  try {
-    date start(from_simple_string(s));
-    date today = day_clock::local_day();
-    days diff = today - start;
-    std::cout << "now: " << today << std::endl;
-    std::cout << "then: " << start << std::endl;
-    std::cout << "diff: " << diff.days() << " days" << std::endl;
-  }
-  catch (...) {
-    std::cout << "bad date: " << s << std::endl;
-  }
+  std::string s1 = "2016-03-30";
+  std::string s2 = "2016-05-13";
+  std::cout << "diff: " << diffDays(s1, s2) << " days" << std::endl;
   return 0;
 }
