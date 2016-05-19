d8e54f47a417f5a40d16b7332b1da23b67fc8a4e
Michael Madden <mikemadden42@icloud.com>
2016-05-19 11:18:07 -0500

    Changed end date.

--- delta.cc
+++ delta.cc
@@ -12,7 +12,7 @@ int diffDays(std::string s1, std::string s2) {
 
 int main() {
   std::string s1 = "2016-03-30";
-  std::string s2 = "2016-05-13";
+  std::string s2 = to_simple_string(day_clock::local_day());
   std::cout << "diff: " << diffDays(s1, s2) << " days" << std::endl;
   return 0;
 }
