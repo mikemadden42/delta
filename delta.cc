56d233582dcca7314e08e1541ff4b00e82f5bc20
Michael Madden <mikemadden42@icloud.com>
2017-09-17 14:22:12 -0500

    Clean up with clang-tidy

--- delta.cc
+++ delta.cc
@@ -4,15 +4,15 @@
 using namespace boost::gregorian;
 
 int diffDays(std::string s1, std::string s2) {
-  date start(from_simple_string(s1));
-  date end(from_simple_string(s2));
-  days diff = start - end;
-  return std::abs(diff.days());
+    date start(from_simple_string(s1));
+    date end(from_simple_string(s2));
+    days diff = start - end;
+    return std::abs(diff.days());
 }
 
 int main() {
-  std::string s1 = "2016-03-30";
-  std::string s2 = to_simple_string(day_clock::local_day());
-  std::cout << "diff: " << diffDays(s1, s2) << " days" << std::endl;
-  return 0;
+    std::string s1 = "2016-03-30";
+    std::string s2 = to_simple_string(day_clock::local_day());
+    std::cout << "diff: " << diffDays(s1, s2) << " days" << std::endl;
+    return 0;
 }
